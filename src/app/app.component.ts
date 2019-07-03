70ed7470cdbcf5887e9d9f288e357100fb94d5be
Nomhleh <ayandanomhle1@gmail.com>
2019-07-03 22:02:29 +0200

    tryone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'apptwo';
+
+  InputText:string="Yolanda";
+  putontext:string ="Jones";
+  number:number=20;
+  
+  pressHere(){ 
+
+    console.log (this.InputText)
+    console.log (this.putontext)
+    console.log (this.number)
+  }
+  
+
 }
+
